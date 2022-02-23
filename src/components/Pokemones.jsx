0397759f03da5e5d8bd93d64b860ea6c2cf837d6
samdev <samuelberrus@gmail.com>
2022-02-23 11:41:43 +0100

    Utilizando el getState() y enviando argumentos desde el componente al action

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Dispatch y selector son los metodos que nos permiten interactuar y leer datos de la store
 import { useDispatch, useSelector } from "react-redux";
 
 // Importamos la acción que deseamos usar de los redux de la store.
-import { obtenerPokemonesAccion } from "../redux/pokeDucks";
+import { obtenerPokemonesAccion, siguientePaginaAction } from "../redux/pokeDucks";
 
 const Pokemones = () => {
 	// Instanciamos dispatch para poder interactuar con la store.
 	const dispatch = useDispatch();
 
+	// Instanciamos useSelector para poder leer los datos de la store.
+	// useSelector recibe un callback que recibe la store y nosotros podemos
+	// indicar a que datos de la store queremos acceder
+	const pokemones = useSelector((store) => {
+		// Accedemos al array dentro de la propiedad pokemones de la store
+		return store.pokemones.array;
+	});
+
+	const offset = useSelector((store) => {
+		// Accedemos al array dentro de la propiedad pokemones de la store
+		return store.pokemones.offset;
+	});
+
 	return (
 		<div>
 			<h1>Lista de pokemones</h1>
+
 			<button
 				onClick={() => {
 					// Ejecutamos en el argumento la acción que deseemos ejecutar del reducer.
@@ -22,6 +36,26 @@ const Pokemones = () => {
 			>
 				Get Pokemones
 			</button>
+
+			<button
+				onClick={() => {
+					// Ejecutamos en el argumento la acción que deseemos ejecutar del reducer.
+					// En este caso usamos la acción "obtenerPokemonesAccion" definida en el pokePato.
+
+					// Podemos enviar argumentos que recibirá la primera función flecha de los actions
+					dispatch(siguientePaginaAction(20));
+				}}
+			>
+				Siguiente
+			</button>
+
+			<ol>
+				{pokemones.map((pokemon) => (
+					<li key={pokemon.name}>
+						<a href={pokemon.url}>{pokemon.name}</a>
+					</li>
+				))}
+			</ol>
 		</div>
 	);
 };
