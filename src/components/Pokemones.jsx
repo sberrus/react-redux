c00e88d49d64fefe9b79c22f3e3aa0074ed6025f
samdev <samuelberrus@gmail.com>
2022-02-23 11:58:11 +0100

    Utilizando los datos que nos ofrece la API

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // Importamos la acción que deseamos usar de los redux de la store.
-import { obtenerPokemonesAccion, siguientePaginaAction } from "../redux/pokeDucks";
+import { obtenerPokemonesAccion, siguientePaginaAction, retrocederPaginaAction } from "../redux/pokeDucks";
 
 const Pokemones = () => {
 	// Instanciamos dispatch para poder interactuar con la store.
@@ -14,41 +14,37 @@ const Pokemones = () => {
 	// useSelector recibe un callback que recibe la store y nosotros podemos
 	// indicar a que datos de la store queremos acceder
 	const pokemones = useSelector((store) => {
-		// Accedemos al array dentro de la propiedad pokemones de la store
-		return store.pokemones.array;
+		return store.pokemones.results;
 	});
 
 	const offset = useSelector((store) => {
-		// Accedemos al array dentro de la propiedad pokemones de la store
 		return store.pokemones.offset;
 	});
 
 	return (
 		<div>
 			<h1>Lista de pokemones</h1>
-
 			<button
 				onClick={() => {
-					// Ejecutamos en el argumento la acción que deseemos ejecutar del reducer.
-					// En este caso usamos la acción "obtenerPokemonesAccion" definida en el pokePato.
+					dispatch(retrocederPaginaAction());
+				}}
+			>
+				Anterior
+			</button>{" "}
+			<button
+				onClick={() => {
 					dispatch(obtenerPokemonesAccion());
 				}}
 			>
 				Get Pokemones
-			</button>
-
+			</button>{" "}
 			<button
 				onClick={() => {
-					// Ejecutamos en el argumento la acción que deseemos ejecutar del reducer.
-					// En este caso usamos la acción "obtenerPokemonesAccion" definida en el pokePato.
-
-					// Podemos enviar argumentos que recibirá la primera función flecha de los actions
-					dispatch(siguientePaginaAction(20));
+					dispatch(siguientePaginaAction());
 				}}
 			>
 				Siguiente
 			</button>
-
 			<ol>
 				{pokemones.map((pokemon) => (
 					<li key={pokemon.name}>
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -2,21 +2,26 @@ import axios from "axios";
 
 //Constantes
 const dataInicial = {
-	array: [],
-	offset: 0,
+	count: 0,
+	next: null,
+	previous: null,
+	results: [],
 };
 
 //TYPES
 const OBTENER_POKEMONES_EXITO = "OBTENER_POKEMONES_EXITO";
 const SIGUIENTE_PAGINA_EXITO = "SIGUIENTE_PAGINA_EXITO";
+const RETROCEDER_PAGINA_EXITO = "RETROCEDER_PAGINA_EXITO";
 
 //Reducers
 export default function pokeReducer(state = dataInicial, action) {
 	switch (action.type) {
 		case OBTENER_POKEMONES_EXITO:
-			return { ...state, array: action.payload };
+			return { ...state, ...action.payload };
 		case SIGUIENTE_PAGINA_EXITO:
-			return { ...state, array: action.payload.array, offset: action.payload.offset };
+			return { ...state, ...action.payload };
+		case RETROCEDER_PAGINA_EXITO:
+			return { ...state, ...action.payload };
 		default:
 			return state;
 	}
@@ -25,38 +30,36 @@ export default function pokeReducer(state = dataInicial, action) {
 //Actions
 export const obtenerPokemonesAccion = () => async (dispatch) => {
 	try {
-		//Llamamos a los datos con los que vamos a trabajar
 		const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`);
 
-		//dispatch es el que se encarga de llevar le objeto con los datos desde las actions al reducer.
-		// la propiedad type es el que se encarga de identificar que reducer vamos a ocupar.
-		// y el payload es los datos que queremos llevar al store.
-		dispatch({ type: OBTENER_POKEMONES_EXITO, payload: res.data.results });
+		dispatch({ type: OBTENER_POKEMONES_EXITO, payload: res.data });
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-//Notese que podemos enviar argumenbtos desde el componente React para que podamos usarlo en el
-//action. En este caso enviamos un argumento número que se utiliza en la lógica del action para hacer
-// la interacción
-export const siguientePaginaAction =
-	(numero = 0) =>
-	async (dispatch, getState) => {
-		// estado actual del offset
-		const { offset } = getState().pokemones;
+export const siguientePaginaAction = () => async (dispatch, getState) => {
+	//Accedemos a las urls que nos devuelve la API
+	const nextUrl = getState().pokemones.next;
+	try {
+		// http
+		const res = await axios.get(nextUrl);
 
-		// nuevo offset que vamos a utilizar
-		const siguiente = offset + numero;
+		dispatch({ type: SIGUIENTE_PAGINA_EXITO, payload: res.data });
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const retrocederPaginaAction = () => async (dispatch, getState) => {
+	//Accedemos a las urls que nos devuelve la API
+	const backUrl = getState().pokemones.previous;
 
-		try {
-			// http
-			const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${siguiente}&limit=20`);
+	try {
+		// http
+		const res = await axios.get(backUrl);
 
-			//En el dispatch en vez de utilizar un dato vamos a enviar un objeto para poder enviar tanto la respuesta del http
-			//como el nuevo offset
-			dispatch({ type: SIGUIENTE_PAGINA_EXITO, payload: { array: res.data.results, offset: siguiente } });
-		} catch (error) {
-			console.log(error);
-		}
-	};
+		dispatch({ type: SIGUIENTE_PAGINA_EXITO, payload: res.data });
+	} catch (error) {
+		console.log(error);
+	}
+};
